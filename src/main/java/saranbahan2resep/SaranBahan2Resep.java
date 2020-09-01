9941f1e19ec9b36abaf0c7a78e3ae6c0c7ab281d
Aizar <Aizar@LAPTOP-S5E5SCG1>
2020-09-01 21:42:44 +0700

    Initial Commit

---
+++ src/main/java/saranbahan2resep/SaranBahan2Resep.java
@@ -0,0 +1,17 @@
+package saranbahan2resep;
+
+/**
+ *
+ * @author Aizar
+ */
+public class SaranBahan2Resep {
+
+    public static void main(String args[]) {
+        String identitas = "Aizar Rahima Suprayitno / X RPL 4 / 02";
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+        
+    }
+}
